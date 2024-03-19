6fd5485de3ab7dc4884b9170fdba52e9f483adc7
dilbar <dilbarturna@gmail.com>
2024-03-19 13:39:48 -0400

    add back output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // basePath: "/dilbar.github.io",
+    output: "export",  // <=== enables static exports
+    // reactStrictMode: true,
 };
 
 export default nextConfig;

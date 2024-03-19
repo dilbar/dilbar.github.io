0065e4d71a61766659088f605d5152ea068adbc3
dilbar <dilbarturna@gmail.com>
2024-03-19 15:10:04 -0400

    f

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/dilbar.github.io",
+    
     output: 'export',
-    // reactStrictMode: true,
-};
 
-module.exports =  nextConfig;
+  };
+
+  module.exports = nextConfig;
